5dd4e7fe832f20ca8bcc53f32ac5aaa9957e7b7f
Anya Brun <tsbrun@icloud.com>
2020-08-18 14:35:09 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,12 @@
+class Appointment
+  attr_reader :date, :name, :doctor
+
+  @@all = [] 
+
+  def initialize(date, name, doctor)
+    @date = date
+    @name = name
+    @doctor = doctor
+    @@all << self
+  end
+end
