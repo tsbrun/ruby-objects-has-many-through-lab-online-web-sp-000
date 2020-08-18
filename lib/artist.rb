5a341d961cbee9702566b76bca8e24f22c732603
Anya Brun <tsbrun@icloud.com>
2020-08-18 14:24:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,6 +11,7 @@ class Artist
   def songs
     # search through Songs.all
     # find the ones were song.artist == self
+    Song.all.select {|song| song.artist == self}
 
     # maintain a single source of truth
     # if you want information about a song, go to the Songs class
